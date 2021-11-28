bc4de58075ef99aea76d6de870cfc9cbf09991da
somegitking <ironvegapentech@gmail.com>
2021-11-28 19:04:41 +0900

    1. 클릭 할때 마다 추첨된 번호 추가후 있는가? (O) 2. div를 동적으로 생성하여 붙여 넣을수 있는가? (O) 3. 추첨된 번호에 스타일적 요소 넣을 수 있는가? ( )

--- lottoGen01.js
+++ lottoGen01.js
@@ -36,7 +36,35 @@ drawnNos.sort((a, b) => {
 })
     console.log(drawnNos)
 
-    let a = document.getElementById("lottoNums")
-    a.innerHTML = `<p>${drawnNos}</p>`
+//    const newDiv = document.getElementById("lottoNums")
+
+    const newDiv = document.createElement('div')
+
+    const newNums = document.createTextNode(drawnNos)
+
+    newDiv.appendChild(newNums)
+
+    document.body.appendChild(newDiv)
+
+    // function createDiv() {
+    //     // 1. <div> element 만들기
+    //     const newDiv = document.createElement('div');
+    //
+    //     // 2. <div>에 들어갈 text node 만들기
+    //     const newText = document.createTextNode('안녕하세요');
+    //
+    //     // 3. <div>에 text node 붙이기
+    //     newDiv.appendChild(newText);
+    //
+    //     // 4. <body>에 1에서 만든 <div> element 붙이기
+    //     document.body.appendChild(newDiv);
+    // }
+
+
+
+
+
+
+
 }
 
